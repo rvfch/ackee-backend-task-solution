03699674ff3de741461b5343ab10515c242b00a3
Roman Filatov <rvfchdev@gmail.com>
2023-08-27 16:25:59 +0200

    ✨ Add API versioning

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
-import { createServer } from 'unicore'
+import { createRouter, createServer } from 'unicore'
 import { ctrl } from './app/controllers'
 import pino from 'pino-http'
 import * as problem from './app/services/problemService'
@@ -17,36 +17,40 @@ server.use(ctrl.cors)
 server.all('/', ctrl.httpRootHandler)
 server.use(ctrl.healthz)
 
-server.get(
+const router = createRouter()
+
+router.get(
   '/problems',
   validate({ body: problemInputOnlyType }),
   ctrl.service(problem.list)
 )
-server.post(
+router.post(
   '/problems',
   validate({ body: problemInput }),
   ctrl.service(problem.create)
 )
-server.get(
+router.get(
   '/problems/:id',
   validate({ body: problemInputOnlyType, params: idRequired }),
   ctrl.service(problem.getOne)
 )
-server.put(
+router.put(
   '/problems/:id',
   validate({ body: problemInput, params: idRequired }),
   ctrl.service(problem.update)
 )
-server.delete(
+router.delete(
   '/problems/:id',
   validate({ params: idRequired }),
   ctrl.service(problem.destroy)
 )
-server.post(
+router.post(
   '/problems/:id/answer',
   validate({ body: answerBody, params: idRequired }),
   ctrl.service(problem.solve)
 )
+
+server.use('/api/v1', router)
 
 server.use(ctrl.httpErrorHandler)
 server.use(ctrl.httpFinalHandler)
